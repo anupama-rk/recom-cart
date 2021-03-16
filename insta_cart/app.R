d1c650244079be53ff634a1c0f00872bd0219c9c
anupama-rk <anupamark@gmail.com>
2021-03-15 22:33:47 -0400

    adding pics and updated shiny

--- insta_cart/app.R
+++ insta_cart/app.R
@@ -11,7 +11,7 @@ library(shiny)
 library(DT)
 
 # Define UI for application that draws a histogram
-ui <- navbarPage("Smart Cart",
+shinyApp(ui <- navbarPage("Smart Cart",
                  tabPanel("Home",
                           
                           fluidRow(
@@ -21,24 +21,44 @@ ui <- navbarPage("Smart Cart",
                           tabsetPanel(
                           type="tabs",
                           tabPanel("Top Selling",
-                            mainPanel(DTOutput('table'))),
+                           # mainPanel(DTOutput('table'))),
+                           
+                            mainPanel(
+                                # actionButton("p1Button", "Produce"),
+                                # actionButton("p2Button","Frozen"),
+                                # actionButton("p3Button","Personal Care"),
+                                bsCollapse(id = "collapseExample", open = "Panel 2",
+                                           bsCollapsePanel("Produce", "This is a panel with just text ",
+                                                           "and has the default style. You can change the style in ",
+                                                           "the sidebar.", DTOutput('table1'),style = "info"),
+                                           bsCollapsePanel("Frozen", "This panel has a generic plot. ",
+                                                           "and a 'success' style.", DTOutput('table2'), style = "success"),
+                                           bsCollapsePanel("Personal Care", "This panel has a generic plot. ",
+                                                           "and a 'success' style.", DTOutput('table3'), style = "info")
+                                ))),
                           tabPanel("Orders"),
                           tabPanel("Confirmation")
                           
                           )
 ),
-                tabPanel("About Us"),
-                tabPanel("Contact Us")
-)
+                tabPanel("About Us",
+                         h3("Why to associate with Smart cart?"),
+                         br(),
+                         h5("Smart Cart is a tool that will help with your online grocery orders.","Smart cart recommends you with products based on purchase history.","Never come home from shopping just to realize that you forgot to buy pancake mix, olive oil and first aid. ")),
+                tabPanel("Contact Us",
+                         a(actionButton(inputId = "email1", label = "Send Email", 
+                                        icon = icon("envelope", lib = "font-awesome")),
+                           href="mailto:admin@smartcart.com"))
+),
                  
 
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output,session) {
 
     products<-read.csv("products.csv")
     head(products)
-    top_prod<-products[1:50,1:2]
+    top_prod<-products[1:5,1:2]
     
    # output$tbl <- renderDataTable(top_prod, options = list(
     #     pageLength = 5,
@@ -47,7 +67,7 @@ server <- function(input, output) {
     #)
     
     
-    output$table <- renderDT({
+    output$table1 <- renderDT({
         data <- data.frame(ROW = top_prod$product_name,
                            TEXT = '<input id="text" type="text" class="form-control" value=""/>',
                            SINGLE_SELECT = '<select id="single_select" style="width: 100%;">
@@ -63,7 +83,55 @@ server <- function(input, output) {
                   rownames = FALSE)
     })
     
+    
+    output$table2 <- renderDT({
+        data <- data.frame(ROW = top_prod$product_name,
+                           TEXT = '<input id="text" type="text" class="form-control" value=""/>',
+                           SINGLE_SELECT = '<select id="single_select" style="width: 100%;">
+                                          <option value="" selected></option>
+                                          <option value="A">A</option>
+                                          <option value="B">B</option>
+                                          <option value="C">C</option>
+                                        </select>',stringsAsFactors = FALSE)
+        
+        datatable(data = data,
+                  selection = "none",
+                  escape = FALSE,
+                  rownames = FALSE)
+    })
+    output$table3 <- renderDT({
+        data <- data.frame(ROW = top_prod$product_name,
+                           TEXT = '<input id="text" type="text" class="form-control" value=""/>',
+                           SINGLE_SELECT = '<select id="single_select" style="width: 100%;">
+                                          <option value="" selected></option>
+                                          <option value="A">A</option>
+                                          <option value="B">B</option>
+                                          <option value="C">C</option>
+                                        </select>',stringsAsFactors = FALSE)
+        
+        datatable(data = data,
+                  selection = "none",
+                  escape = FALSE,
+                  rownames = FALSE)
+    })
+    
+    output$genericPlot <- renderPlot(plot(rnorm(100)))
+    observeEvent(input$p1Button, ({
+        updateCollapse(session, "collapseExample", open = "Produce")
+    }))
+    
+    observeEvent(input$p2Button, ({
+        updateCollapse(session, "collapseExample", open = "Frozen")
+    }))
+    
+    observeEvent(input$p3Button, ({
+        updateCollapse(session, "collapseExample", open = "Personal Care")
+    }))
+    
+    
+    
+    
 }
-
+)
 # Run the application 
-shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, server = server)
